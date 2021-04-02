979bd40da6d9140978670be5adf97bcd6d90dd46
AlbertoWindows <albertopimentel04@gmail.com>
2021-04-01 22:47:33 -0600

    primer uso de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ["./public/**/*.html"], //Aplicar el procesamiento post css
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "wave-pattern": "url(../img/wave-white.png)",
+        "wave-pattern-dots": "url(../img/pattern-white-dots.png)",
+        "pattern-characters-red": "url(../img/pattern-characters-red.png)",
+        "character-mario": "url(../img/character-l.png)",
+        "wave-pink": "url(../img/wave-pink.png)",
+        "yellow-dots": "url(../img/pattern-yellow-dots.png)",
+      }),
+      screens: {
+        xsm: "320px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
